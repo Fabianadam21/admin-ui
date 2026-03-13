3abd78710f2ad715365d5fb68ffd9d624cb36549
Fabianadam21 <rrqfabian07@gmail.com>
2026-03-13 12:51:43 +0700

    ubah teks di app

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,8 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
+      <h1>Vite + React</h1>
+      <h3>Fabian Adam Maheswara</h3>
       <h1 className=" bg-blue-200">Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
